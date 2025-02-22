ef411c63f73cf6f0a16420ef8e4fb053fe65a259
Olha <oyashnaya88@gmail.com>
2025-02-22 11:28:28 +0200

    task-3

---
+++ src/scripts/addOneProduct.js
@@ -0,0 +1,11 @@
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/products.js';
+import { createFakeProduct } from '../utils/createFakeProduct.js';
+
+const addOneProduct = async () => {
+        const productList = await fs.readFile(PATH_DB);
+        const productListParse = JSON.parse(productList);
+        productListParse.push(createFakeProduct());
+        await fs.writeFile(PATH_DB, JSON.stringify(productListParse, null, 2), 'utf-8')
+}
+addOneProduct();
